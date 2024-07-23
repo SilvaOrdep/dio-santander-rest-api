3796efd6d7cf0be1701b9b255ecb1516310cc5e1
Pedro Silva <pedro.o.silva975@gmail.com>
2024-07-23 12:46:21 -0300

    new archives

---
+++ src/main/java/study/model/Feature.java
@@ -0,0 +1,7 @@
+package study.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name="tb_features")
+public class Feature extends StandardModel {
+}
---
+++ src/main/java/study/model/News.java
@@ -0,0 +1,7 @@
+package study.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name="tb_news")
+public class News extends StandardModel {
+}
